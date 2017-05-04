030e35219d38e7c9cb4ddcc41b1cd593fbc859af
shan-bao.qi@hpe.com <shan-bao.qi@hpe.com>
2017-05-04 16:48:18 +0800

    US#79010-epic-wizard-step3-octane-code: create epic in octane; get epic pahse api; get epic parent api;

---
+++ src/com/ppm/integration/agilesdk/connector/octane/model/EpicAttr.java
@@ -0,0 +1,20 @@
+package com.ppm.integration.agilesdk.connector.octane.model;
+
+public class EpicAttr extends SimpleEntity {
+    
+  	public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(final String type) {
+        this.type = type;
+    }
+}
