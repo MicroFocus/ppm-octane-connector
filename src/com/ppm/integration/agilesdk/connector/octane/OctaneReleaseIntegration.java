39a5f34b1bfa50f5540b27524cf1b301ba81b62f
LuChunQi <chunqi_lu@126.com>
2017-05-15 11:14:15 +0800

    split creating epic info to OctaneReleaseIntegration.java

--- src/com/ppm/integration/agilesdk/connector/octane/OctaneReleaseIntegration.java
+++ src/com/ppm/integration/agilesdk/connector/octane/OctaneReleaseIntegration.java
@@ -20,7 +20,6 @@ import com.ppm.integration.agilesdk.release.ReleaseRelease;
 import com.ppm.integration.agilesdk.release.ReleaseReleaseTeam;
 import com.ppm.integration.agilesdk.release.ReleaseSprint;
 import com.ppm.integration.agilesdk.release.ReleaseTheme;
-import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
 import org.json.JSONException;
 
@@ -380,32 +379,4 @@ public class OctaneReleaseIntegration extends ReleaseIntegration {
         }
         return epic;
     }
-
-    @Override public Long createEpicInWorkspace(final String sharedSpaceId, final String workSpaceId, final EpicCreateInfo epicCreateInfo, final ValueSet paramValueSet) {
-        Long epicId = null;
-        client = getClient(paramValueSet);
-        EpicEntity epic = new EpicEntity();
-        epic.setName(epicCreateInfo.getName());
-        
-        try {
-            List<EpicAttr> epicPhases = client.getEpicPhase(sharedSpaceId, workSpaceId, "phase.epic.new");
-            epic.setPhase(epicPhases.get(0));
-            
-            List<EpicAttr> epicParents = client.getEpicParent(sharedSpaceId, workSpaceId, "work_item_root");
-            epic.setParent(epicParents.get(0));
-            List<EpicEntity> epicList = new ArrayList<EpicEntity>();
-            epicList.add(epic);
-            
-            EpicCreateEntity epicCreateEntity = new EpicCreateEntity();
-            epicCreateEntity.setData(epicList);
-        	
-            List<EpicEntity> epics = client.createEpicInWorkspace(sharedSpaceId, workSpaceId, epicCreateEntity);
-            epicId = epics.size() > 0 ? Long.valueOf(epics.get(0).id) : null;
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
-        
-        return epicId;
-    }
-
 }
