fe525ea98b6411efcd30e2f9124f98cef12a1d10
bing-chen.li <bing-chen.li@hpe.com>
2017-05-26 16:28:02 +0800

    reformat code

--- src/com/ppm/integration/agilesdk/connector/octane/OctanePortfolioEpicIntegration.java
+++ src/com/ppm/integration/agilesdk/connector/octane/OctanePortfolioEpicIntegration.java
@@ -2,6 +2,7 @@ package com.ppm.integration.agilesdk.connector.octane;
 
 import com.hp.ppm.integration.model.Workspace;
 import com.ppm.integration.agilesdk.ValueSet;
+import com.ppm.integration.agilesdk.agiledata.AgileDataTheme;
 import com.ppm.integration.agilesdk.connector.octane.client.ClientPublicAPI;
 import com.ppm.integration.agilesdk.connector.octane.client.OctaneClientException;
 import com.ppm.integration.agilesdk.connector.octane.model.EpicAttr;
@@ -12,14 +13,12 @@ import com.ppm.integration.agilesdk.connector.octane.model.WorkItemEpic;
 import com.ppm.integration.agilesdk.connector.octane.model.WorkSpace;
 import com.ppm.integration.agilesdk.epic.AgileProject;
 import com.ppm.integration.agilesdk.epic.PortfolioEpicIntegration;
-import com.ppm.integration.agilesdk.agiledata.AgileDataTheme;
+import java.util.ArrayList;
+import java.util.List;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by luch on 5/9/2017.
@@ -27,15 +26,17 @@ import java.util.List;
 public class OctanePortfolioEpicIntegration extends PortfolioEpicIntegration {
 
     private final Logger logger = Logger.getLogger(this.getClass());
-    private static final String DEFAULT_OCTANE_EPIC_URL = "/ui/entity-navigation?p={sharedSpaceId}/{workSpaceId}&entityType=work_item&id={epicId}";
+
+    private static final String DEFAULT_OCTANE_EPIC_URL =
+            "/ui/entity-navigation?p={sharedSpaceId}/{workSpaceId}&entityType=work_item&id={epicId}";
 
     /**
      * Get all available agile projects(agile workspaces) in a instance
-     * @param paramValueSet  a value set which contains the information of the instance
+     *
+     * @param paramValueSet a value set which contains the information of the instance
      * @return a list of agile project
      */
-    @Override
-    public List<AgileProject> getAgileProjects(ValueSet paramValueSet) {
+    @Override public List<AgileProject> getAgileProjects(ValueSet paramValueSet) {
         List<AgileProject> agileProjectList = new ArrayList();
         try {
             ClientPublicAPI client = OnctaneIntegrationHelper.getClient(paramValueSet);
@@ -65,13 +66,15 @@ public class OctanePortfolioEpicIntegration extends PortfolioEpicIntegration {
 
     /**
      * Create portfolio epic under the agile project(agile workspace) in a instance
-     * @param epicName the name of epic
-     * @param value this is a json format value, which contains the necessary information to create epic.
+     *
+     * @param epicName      the name of epic
+     * @param value         this is a json format value, which contains the necessary information to create epic.
      * @param paramValueSet a value set which contains the information of the instance
      * @return epic id
      */
-    @Override
-    public Long createEpicInAgileProject(final String epicName, final String value, final ValueSet paramValueSet) {
+    @Override public Long createEpicInAgileProject(final String epicName, final String value,
+            final ValueSet paramValueSet)
+    {
         Long epicId = null;
         EpicEntity epic = new EpicEntity();
 
@@ -105,25 +108,22 @@ public class OctanePortfolioEpicIntegration extends PortfolioEpicIntegration {
         return epicId;
     }
 
-
-    @Override
-    public AgileDataTheme getEpicInfo(final Workspace wp, final ValueSet values, final String epicId, final String jsonText) {
+    @Override public AgileDataTheme getEpicInfo(final Workspace wp, final ValueSet values, final String epicId,
+            final String jsonText)
+    {
         AgileDataTheme epic = new AgileDataTheme();
-        try{
+        try {
             ClientPublicAPI client = OnctaneIntegrationHelper.getClient(values);
             JSONObject jsonConfig = (JSONObject)JSONSerializer.toJSON(jsonText);
             String valueText = jsonConfig.getString("value");
             JSONObject valueJson = (JSONObject)JSONSerializer.toJSON(valueText);
             int shareSpaceId = valueJson.getInt("SHARED_SPACE_ID");
             int workSpaceId = valueJson.getInt("WORKSPACE_ID");
-            String[] doneStatusIDs = client.getDoneDefinationOfUserStoryAndDefect(
-                    shareSpaceId, workSpaceId);
+            String[] doneStatusIDs = client.getDoneDefinationOfUserStoryAndDefect(shareSpaceId, workSpaceId);
 
-            WorkItemEpic epic1= client.getEpicActualStoryPointsAndPath(
-                    shareSpaceId, workSpaceId, epicId);
+            WorkItemEpic epic1 = client.getEpicActualStoryPointsAndPath(shareSpaceId, workSpaceId, epicId);
 
-            WorkItemEpic epic2 = client.getEpicDoneStoryPoints(shareSpaceId,
-                    workSpaceId, epic1.path, doneStatusIDs);
+            WorkItemEpic epic2 = client.getEpicDoneStoryPoints(shareSpaceId, workSpaceId, epic1.path, doneStatusIDs);
 
             epic.setDoneStoryPoints(epic2.doneStoryPoints);
             epic.setTotalStoryPoints(epic1.totalStoryPoints);
@@ -135,12 +135,12 @@ public class OctanePortfolioEpicIntegration extends PortfolioEpicIntegration {
 
     /**
      * Get the epic URI, which is used to generate a link of epic in PPM.
+     *
      * @param epicId epic id in agile tool
-     * @param value this is a json format value, which contains the necessary information to create epic.
+     * @param value  this is a json format value, which contains the necessary information to create epic.
      * @return epic URI
      */
-    @Override
-    public String getEpicURI(final Long epicId, final String value) {
+    @Override public String getEpicURI(final Long epicId, final String value) {
         JSONObject valueJson = (JSONObject)JSONSerializer.toJSON(value);
         String epicUrl = DEFAULT_OCTANE_EPIC_URL;
         epicUrl = StringUtils.replace(epicUrl, "{sharedSpaceId}", valueJson.getString("SHARED_SPACE_ID"));
