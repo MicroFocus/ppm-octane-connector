6c1dc577fcc50d5ddbed1aee96a76b9ebeeacb14
Wen Rui He <wen-rui.he@hpe.com>
2019-01-29 17:01:19 +0800

    DEFECT#309092 The Agile field type reference need to be redefined by the type of its field_type_data attribut_change_param_name

--- src/com/ppm/integration/agilesdk/connector/octane/OctaneConstants.java
+++ src/com/ppm/integration/agilesdk/connector/octane/OctaneConstants.java
@@ -167,8 +167,8 @@ public class OctaneConstants {
     public static final String OCTANE_FIELD_TYPE_STRING = "OCTANE_FIELD_TYPE_STRING";
     public static final String OCTANE_FIELD_TYPE_MEMO = "OCTANE_FIELD_TYPE_MEMO";
     public static final String OCTANE_FIELD_TYPE_USER_LIST = "OCTANE_FIELD_TYPE_USER_LIST";
-    public static final String OCTANE_FIELD_TYPE_SUB_TYPE_LIST_NODE = "OCTANE_FIELD_TYPE_SUB_TYPE_LIST_NODE";
-    public static final String OCTANE_FIELD_TYPE_AUTO_COMPLETE_LIST = "OCTANE_FIELD_TYPE_AUTO_COMPLETE_LIST";
+    public static final String OCTANE_FIELD_TYPE_LIST = "OCTANE_FIELD_TYPE_LIST";
+    public static final String OCTANE_FIELD_TYPE_REFERENCE = "OCTANE_FIELD_TYPE_REFERENCE";
 
     public static final String SSO_IDENTIFIER = "SSO_IDENTIFIER";
 
