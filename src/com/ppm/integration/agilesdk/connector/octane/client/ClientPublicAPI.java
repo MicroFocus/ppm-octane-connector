6257bd5c61b7da55d91d7e0934e2faa6f9029485
Wen Rui He <wen-rui.he@hpe.com>
2019-04-12 14:53:48 +0800

    US#340066 PPM hidden fields tobe displayed_remove hidden field filter and add is user defined attribute

--- src/com/ppm/integration/agilesdk/connector/octane/client/ClientPublicAPI.java
+++ src/com/ppm/integration/agilesdk/connector/octane/client/ClientPublicAPI.java
@@ -1023,7 +1023,7 @@ public class ClientPublicAPI {
     public List<FieldInfo> getEntityFields(final String sharedspaceId, final String workspaceId, final String entityName) {
         String url = String.format("%s/api/shared_spaces/%s/workspaces/%s/metadata/fields?query=%s%s%s",
                 baseURL, sharedspaceId, workspaceId, "%22entity_name%20EQ%20'", entityName,
-                "';visible_in_ui%20EQ%20true;editable%20EQ%20true;field_type%20IN%20'string','reference','memo','integer'%22");
+                "';editable%20EQ%20true;field_type%20IN%20'string','reference','memo','integer'%22");
         // "';visible_in_ui%20EQ%20true;editable%20EQ%20true;field_type%20IN%20'string','reference','memo'%22");
         List fieldsList = new ArrayList();
         RestResponse response = sendGet(url);
