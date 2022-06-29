03509a9fb28133d9ce88a11961214d05ace112b3
Elva Zhu <dong-hui.zhu@microfocus.com>
2022-06-29 16:14:00 +0800

    US#436010 fix duplicate shared epic to PPM

--- src/com/ppm/integration/agilesdk/connector/octane/OctaneRequestIntegration.java
+++ src/com/ppm/integration/agilesdk/connector/octane/OctaneRequestIntegration.java
@@ -385,6 +385,9 @@ public class OctaneRequestIntegration extends RequestIntegration {
                     workspaceIds.add(OctaneConstants.SHARED_EPIC_DEFAULT_WORKSPACE);
                 } else {
                     workspaceIds = client.getApiAccessWorkSpaces(Integer.parseInt(sharedSpaceId));
+                    // remove the default master workspace that belong to the
+                    // current space as it can only create shared epic
+                    workspaceIds.remove(OctaneConstants.SHARED_EPIC_DEFAULT_WORKSPACE);
                 }
             }
         } else {
@@ -1198,8 +1201,11 @@ public class OctaneRequestIntegration extends RequestIntegration {
     private List<IdProjectDate> getNewCreatedEntities(ClientPublicAPI client, String spaceId, String workSpaceId,
             String entityType, Date creationDate)
     {
+        // if it is shared epic,its real workspace id is 500.But it could
+        // navigation to using the mask workspace id
+        String realWorkspaceId = workSpaceId;
         if (OctaneConstants.SUB_SHARED_EPIC.equals(entityType)) {
-            workSpaceId = OctaneConstants.SHARED_EPIC_DEFAULT_WORKSPACE;
+            realWorkspaceId = OctaneConstants.SHARED_EPIC_DEFAULT_WORKSPACE;
             entityType = OctaneConstants.SUB_TYPE_EPIC;
         }
 
@@ -1207,22 +1213,33 @@ public class OctaneRequestIntegration extends RequestIntegration {
         if (null != creationDate) {
             queryParams.put("creation_time", creationDate);
         }
+        queryParams.put("subtype", entityType);
         List<String> fields = new ArrayList<>();
         fields.add("id");
         fields.add("creation_time");
+        fields.add("workspace_id");
         List<JSONObject> itemJson =
-                client.getWorkItems(spaceId, workSpaceId, ClientPublicAPI.EntityType.fromName(entityType), queryParams,
+                client.getWorkItems(spaceId, realWorkspaceId, ClientPublicAPI.EntityType.fromName(entityType),
+                        queryParams,
                         fields);
 
-        return constructIdProjectDate(itemJson, spaceId, workSpaceId);
+        return constructIdProjectDate(itemJson, spaceId, workSpaceId, realWorkspaceId);
     }
 
-    private List<IdProjectDate> constructIdProjectDate(List<JSONObject> items, String spaceId, String workspaceId) {
+    private List<IdProjectDate> constructIdProjectDate(List<JSONObject> items, String spaceId, String workspaceId,
+            String realWorkspaceId)
+    {
         List<IdProjectDate> entities = new ArrayList<>();
         JSONObject workspaceJson = new JSONObject();
         workspaceJson.put(OctaneConstants.WORKSPACE_ID, Integer.parseInt(workspaceId));
         workspaceJson.put(OctaneConstants.SHARED_SPACE_ID, Integer.parseInt(spaceId));
         for (JSONObject obj : items) {
+            // if the entity is epic, items contains shared epic.Exclude them
+            String entityWorkspace = obj.getString("workspace_id");
+            if (!OctaneConstants.SHARED_EPIC_DEFAULT_WORKSPACE.equals(realWorkspaceId)
+                    && OctaneConstants.SHARED_EPIC_DEFAULT_WORKSPACE.equals(entityWorkspace)) {
+                continue;
+            }
             entities.add(new IdProjectDate(obj.getString("id"), workspaceJson.toString(),
                     parserDate(obj.getString("creation_time"))));
         }
